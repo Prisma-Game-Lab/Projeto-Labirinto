b429f191775566eaf27086dafcd44fb774d88fa9
tati99 <33660799+tati99@users.noreply.github.com>
2020-10-19 13:47:52 -0300

    Movimentação do player

---
+++ Shopping do Terror/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+        if (inputX != 0 && inputY != 0) {
+            inputX *= 0.5f;
+            inputY *= 0.5f;
+        }
+        rb.velocity = new Vector2(inputX * speed, inputY * speed);
+    }
+}
