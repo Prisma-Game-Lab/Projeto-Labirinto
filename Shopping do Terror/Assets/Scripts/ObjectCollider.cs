47e3a2dee756626193e71a83f808c7a6a70e46e5
tati99 <33660799+tati99@users.noreply.github.com>
2020-10-22 13:16:54 -0300

    Interação com objetos

---
+++ Shopping do Terror/Assets/Scripts/ObjectCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollider : MonoBehaviour
+{
+    public GameObject Object;
+
+    private ObjectCount objectCountScript;
+
+    private List<GameObject> objectList = new List<GameObject>();
+
+    private int count = 0;
+
+    void Start() {
+        objectCountScript = Object.GetComponent<ObjectCount>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag ("Object")) {
+            objectList.Add(other.gameObject);
+            other.gameObject.SetActive(false);
+            count++;
+            objectCountScript.CountText(count);
+        }
+    }
+}
---
+++ Shopping do Terror/Assets/Scripts/ObjectCount.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectCount : MonoBehaviour
+{
+    public Text countText;
+
+    public void CountText(int num) {
+        countText.text = "Objetos: " + num.ToString();
+    }
+
+}
