e9dd45acea326eb53e4f85ebd0103894a95b1a89
Sol <solmseda@gmail.com>
2020-10-30 13:25:58 -0300

    Timer funcionando

---
+++ Shopping do Terror/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float seg = 0;
+    public float min = 0;
+    public float hora = 0;
+
+   void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    void FixedUpdate()
+    {
+        seg += Time.deltaTime;
+
+        if(seg > 60.0f)
+        {
+            seg = 0.0f;
+            min++;
+        }
+        if(min > 60.0f)
+        {
+            min = 0.0f;
+            hora++;
+        }
+    }
+    
+}
