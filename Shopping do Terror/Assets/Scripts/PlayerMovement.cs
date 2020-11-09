918cfd6a39e8c3fad1374ab72a050602d9b459b5
Carol <carol.brandao01@hotmail.com>
2020-11-09 16:41:04 -0300

    adicionando som dos passos

--- Shopping do Terror/Assets/Scripts/PlayerMovement.cs
+++ Shopping do Terror/Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     public float speed;
     private Rigidbody2D rb;
+    AudioSource audioSc;
+    bool isMoving;
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
+        audioSc = GetComponent<AudioSource>();
     }
 
+    void Update()
+    {
+        //checo de o player esta se movendo e atribuo a booleana
+        if (rb.velocity.x != 0 || rb.velocity.y != 0)
+            isMoving = true;
+        else
+            isMoving = false;
+        //caso ele esteja se movendo
+        if (isMoving)
+        {
+            //e o audio ainda n tiver tocando ele toca o audio
+            if (!audioSc.isPlaying)
+            {
+                audioSc.Play();
+            }
+        }
+        else
+            audioSc.Stop();
+
+    }
     void FixedUpdate() {
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
@@ -17,6 +40,7 @@ public class PlayerMovement : MonoBehaviour {
             inputX *= 0.5f;
             inputY *= 0.5f;
         }
+      
         rb.velocity = new Vector2(inputX * speed, inputY * speed);
     }
 }
