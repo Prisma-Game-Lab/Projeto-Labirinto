2cf077aadb80ec1a460b321d648164d0cb99d582
tati99 <33660799+tati99@users.noreply.github.com>
2020-11-21 01:13:53 -0300

    Implementacao inicial do menu

---
+++ Shopping do Terror/Assets/Menu/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("02 - Level 01");
+    }
+
+    public void LoadCreditsScene()
+    {
+        SceneManager.LoadScene("01 - Credits");
+    }
+
+    public void LoadConfigScene()
+    {
+        //SceneManager.LoadScene("");
+    }
+
+    public void LoadControlsScene()
+    {
+        //SceneManager.LoadScene("");
+    }
+    
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
