9c71eac06efa1c61f975f9d6255d09792350259c
tati99 <33660799+tati99@users.noreply.github.com>
2020-12-02 15:36:50 -0300

    Não acrescentei nada novo, só coloquei as cenas no build settings

---
+++ Shopping do Terror/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    //public bool isCheckpoint = false;
+
+    //static private Vector2 initialPosition;
+
+    //private void Start() {
+        
+    //}
+
+    public GameObject Player;
+
+    private void ActivateCheckpoint(Vector2 initialPosition) {
+        //if (Player.trans)
+
+    }
+}
