9349b21022e82d36a27d66bd4e5c3ca3da19227c
Sol <solmseda@gmail.com>
2020-12-07 19:50:04 -0300

    Criação de um spawnPoint geral

--- Shopping do Terror/Assets/Scripts/SpawnPoint.cs
+++ Shopping do Terror/Assets/Scripts/SpawnPoint.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
-    public bool spawnPlayer;
-    public bool spawnIten;
-    [Header("Item ou player a ser spawnado")]
-    public GameObject obj;
+    [Header("Lista de objetos no andar")]
+    public GameObject[] objList;
+
+    [Header("Lista de spawns no andar")]
+    public GameObject[] spawnList;
+
+    private int aux;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (spawnPlayer == true && spawnIten == false)
+        GameObject player = GameObject.FindWithTag("Player");
+        player.transform.position = this.transform.position;
+
+        for( aux = 0; aux < objList.Length; aux++)
         {
-            obj.transform.position = this.transform.position;
-        }
-        else if (spawnIten == true && spawnPlayer == false)
-        {
-            //Instantiate(obj,this.transform);
-            obj.transform.position = this.transform.position;
+            objList[aux].transform.position = spawnList[aux].transform.position;
         }
     }
 
