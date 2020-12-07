fcccbff45714faa5c8706940542f73f344f28235
Sol <solmseda@gmail.com>
2020-12-07 18:13:13 -0300

    Mudança nos cenários para fazer os itens passarem de cena

--- Shopping do Terror/Assets/Scripts/Escada.cs
+++ Shopping do Terror/Assets/Scripts/Escada.cs
@@ -14,29 +14,11 @@ public class Escada : MonoBehaviour
     public GameObject player;
     bool cont = true;
 
-
     void OnCollisionEnter2D(Collision2D other)
     {
-
-        /*
-        //caso o player tenha os itens (que estão no script ObjectCollider) ele carregara a próxima cena
-        for(int i = 0; i < itenList.Count; i++)
-        {
-            if (player.GetComponent<ObjectCollider>().objectListCopy.Contains(itenList[i].name))
-            {
-                cont = true;
-            }
-            else
-            {
-                cont = false;
-                Debug.Log("não tem os itens necessários");
-                //break;
-            }
-            
-        }
-        */
         if (other.gameObject.CompareTag("Player") && cont == true)
         {
+            DontDestroyOnLoad(other.gameObject);
             SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
         }
     }
