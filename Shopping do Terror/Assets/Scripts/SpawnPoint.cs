7ffff691d4b2943202489cbf96970b977281cc29
Sol <solmseda@gmail.com>
2020-12-07 19:04:08 -0300

    Criação de SpawnPoints para itens e player

---
+++ Shopping do Terror/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public bool spawnPlayer;
+    public bool spawnIten;
+    [Header("Item ou player a ser spawnado")]
+    public GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (spawnPlayer == true && spawnIten == false)
+        {
+            obj.transform.position = this.transform.position;
+        }
+        else if (spawnIten == true && spawnPlayer == false)
+        {
+            //Instantiate(obj,this.transform);
+            obj.transform.position = this.transform.position;
+        }
+    }
+
+}
