330dc1c0c5a873345ba2f799fb79c230fb4161d5
mlhomsi <mlhomsi@gmail.com>
2020-12-07 18:56:04 -0300

    Fix Velocity Diagonal

--- Shopping do Terror/Assets/Scripts/PlayerMovement.cs
+++ Shopping do Terror/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour {
     public AudioSource passos;
     bool isMoving;
 
+    private Vector2 adjustOperation;
+
     public Animator playerMov;
 
     void Start() {
@@ -38,11 +40,14 @@ public class PlayerMovement : MonoBehaviour {
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
         if (inputX != 0 && inputY != 0) {
-            inputX *= 0.5f;
-            inputY *= 0.5f;
+         //   inputX *= 0.5f;
+          //  inputY *= 0.5f;
         }
         playerMov.SetFloat("VelX", inputX);
         playerMov.SetFloat("VelY", inputY);
-        rb.velocity = new Vector2(inputX * speed, inputY * speed);
+
+        adjustOperation = new Vector2(inputX * speed, inputY * speed);
+        adjustOperation.Normalize();
+        rb.velocity = adjustOperation*speed;
     }
 }
