cc6c2dad0568b66ee51c6ba80707bab8b593d16b
Carol Thers <carol.brandao01@hotmail.com>
2020-12-11 00:25:07 -0300

    Adicionando sons aleatorios para tocar de 35 em 35 segundos

---
+++ Shopping do Terror/Assets/SoundScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour
+{
+    public AudioSource _as;
+    public AudioClip[] RdSpookySounds;
+    bool PrimeiraVez;
+
+// Start is called before the first frame update
+void Start()
+    {
+        PrimeiraVez = true;
+        StartCoroutine("PlaySound");
+    }
+
+
+    IEnumerator PlaySound()
+    {
+        if (PrimeiraVez == true)
+        {
+            PrimeiraVez = false;
+            yield return new WaitForSeconds(20f);
+        }
+        for (; ; )
+        { 
+         _as.clip = RdSpookySounds[Random.Range(0, RdSpookySounds.Length - 1)];
+         _as.PlayOneShot(_as.clip);
+         yield return new WaitForSeconds(35f);
+        }
+    }
+}
