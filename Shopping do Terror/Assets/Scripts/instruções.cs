37a32149987b6086dda2f2bdbff39741969bb7bc
Sol <solmseda@gmail.com>
2020-12-14 19:45:25 -0300

    instruções script

---
+++ Shopping do Terror/Assets/Scripts/instruções.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instruções : MonoBehaviour
+{
+    void Start()
+    {
+        Time.timeScale = 0.0f;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("e")){
+            Time.timeScale = 1f;
+        }
+    }
+}
