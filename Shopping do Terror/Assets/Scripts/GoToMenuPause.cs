88da51eff3e739d56441d7a2ff92047232c42039
tati99 <33660799+tati99@users.noreply.github.com>
2020-12-20 20:19:32 -0300

    Jogo pronto

---
+++ Shopping do Terror/Assets/Scripts/GoToMenuPause.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GoToMenuPause : MonoBehaviour
+{
+
+    public Image B1;
+    public Image B2;
+    public void LoadScene() {
+        B1.material.color = Color.white;
+        B2.material.color = Color.white;
+        SceneManager.LoadScene("00 - Menu Inicial");
+        Time.timeScale = 1f;
+    }
+}
